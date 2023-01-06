e22feb4fc9735faa7c682f631d075b6034686e11
Didik Setiawan <ds@didiksetiawan.com>
2023-01-07 06:33:58 +0700

    data_visualization: save plots automatically

--- data_vizualization/scatter_squares.py
+++ data_vizualization/scatter_squares.py
@@ -17,4 +17,6 @@ plt.tick_params(axis='both', which='major', labelsize=14)
 # Set the range for each axis
 plt.axis([0, 1100, 0, 1100000])
 
+plt.savefig('squares_plot.out.png', bbox_inches='tight')
+
 plt.show()
