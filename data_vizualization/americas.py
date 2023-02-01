311b7b01bab9bfdba46bdaae84064a135e5e6aa3
Didik Setiawan <ds@didiksetiawan.com>
2023-02-01 10:29:15 +0700

    data_visualization: Fix worldmap on pygal

--- data_vizualization/americas.py
+++ data_vizualization/americas.py
@@ -1,6 +1,6 @@
 import pygal
 
-wm = pygal.Worldmap()
+wm = pygal.maps.world.World()
 wm.title = 'North, Central, and South America'
 
 wm.add('North America', ['ca', 'mx', 'us'])
@@ -8,4 +8,4 @@ wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
 wm.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf',
 	'gy', 'pe', 'py', 'sr', 'uy', 've'])
 
-wm.render_to_file('americas.svg')
+wm.render_to_file('americas.out.svg')
