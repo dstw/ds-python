6847c25d4d9d324a32523199b3fdc3c4862b6ba8
Didik Setiawan <ds@didiksetiawan.com>
2023-02-03 06:00:07 +0700

    data_visualization: Work with the response dictionary

--- data_vizualization/python_repos.py
+++ data_vizualization/python_repos.py
@@ -7,6 +7,22 @@ print("Status code:", r.status_code)
 
 # Store API response in a variable
 response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
 
-# Process results
-print(response_dict.keys())
+# Explore information about the repositories
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+# Examine the first repository
+repo_dict = repo_dicts[0]
+
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Description:', repo_dict['description'])
+
+#print("\nKeys:", len(repo_dict))
+#for key in sorted(repo_dict.keys()):
+#	print(key)
