1e1952121ef091ee944514febd31295d56846040
Didik Setiawan <ds@didiksetiawan.com>
2023-02-03 06:03:50 +0700

    data_visualization: Summarize the top repositories

--- data_vizualization/python_repos.py
+++ data_vizualization/python_repos.py
@@ -13,16 +13,11 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-# Examine the first repository
-repo_dict = repo_dicts[0]
-
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Description:', repo_dict['description'])
-
-#print("\nKeys:", len(repo_dict))
-#for key in sorted(repo_dict.keys()):
-#	print(key)
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+	print("\nSelected information about first repository:")
+	print('Name:', repo_dict['name'])
+	print('Owner:', repo_dict['owner']['login'])
+	print('Stars:', repo_dict['stargazers_count'])
+	print('Repository:', repo_dict['html_url'])
+	print('Description:', repo_dict['description'])
